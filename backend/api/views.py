eca7dce47d33a1376490fa10066ad2b64a04f547
J19012004 <jerminnrebekka8@gmail.com>
2026-07-19 13:23:36 +0530

    Added JWT authentication and React frontend setup

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Student
 from .serializers import StudentSerializer
 
@@ -6,3 +8,4 @@ from .serializers import StudentSerializer
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    permission_classes = [IsAuthenticated]
---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">
+        Smart Student Management System
+      </h1>
+
+      <h3>React + Django + MySQL</h3>
+
+      <p>Frontend is working successfully 🎉</p>
+    </div>
+  );
+}
+
+export default App;
